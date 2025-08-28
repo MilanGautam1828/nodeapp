985c5d4150b0d7ece6c769f3a121379c1c5fcd50
Ubuntu <ubuntu@ip-172-31-18-202.ec2.internal>
2025-08-28 09:32:42 +0000

    Make scripts executable and add create_directory.sh

--- scripts/before_install.sh
+++ scripts/before_install.sh
@@ -1,2 +1,9 @@
-#!/bin/bash
-echo "before install"
+hooks:
+  BeforeInstall:
+    - location: scripts/create_directory.sh
+      timeout: 60
+      runas: ubuntu
+    - location: scripts/stop_server.sh
+      timeout: 60
+      runas: ubuntu
+
---
+++ scripts/create_directory.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+mkdir -p /home/ubuntu/nodeapp
+chown ubuntu:ubuntu /home/ubuntu/nodeapp
+chmod 755 /home/ubuntu/nodeapp
+
